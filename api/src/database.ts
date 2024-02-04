f7be43109c2ec5753f4f5465db5359f196207bfd
Gabriel Reis <gabriel.familia32@hotmail.com>
2024-02-04 17:00:15 -0300

    feat: add adapter Pg

---
+++ api/src/database.ts
@@ -0,0 +1,29 @@
+import { Client } from 'pg'
+
+export default class Pg {
+    private readonly client: Client
+
+    private constructor() {
+        console.log(process.env.POSTGRES_HOST)
+        this.client = new Client({
+            port: 5432,
+            host: process.env.POSTGRES_HOST,
+            user: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DATABASE,
+        })
+
+        Object.freeze(this)
+    }
+
+    static async connect() {
+        const pg = new Pg()
+        pg.client.connect()
+
+        return pg.client
+    }
+
+    async close() {
+        this.client.end()
+    }
+}
